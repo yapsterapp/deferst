a263ea719e0d1ae4dd387409038cb9c36c49ca45
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2016-12-14 14:28:41 +0000

    0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject employeerepublic/deferst "0.2.0"
+(defproject employeerepublic/deferst "0.2.1"
   :description "a simple object system builder"
   :url "http://egithub.com/employeerepublic/deferst"
   :license {:name "Eclipse Public License"
